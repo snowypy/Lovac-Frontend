663c9f9cd35eb5e6732e80339d127159817b1a77
snowypy <git@snowyjs.lol>
2025-01-05 03:17:41 +0000

    fix: add environment variables to image domains for improved flexibility

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['cdn.discordapp.com', 'tickets.minecrush.gg'],
+    domains: ['cdn.discordapp.com', 'tickets.minecrush.gg', `${process.env.NEXT_PUBLIC_LOVAC_BACKEND_URL}`, `${process.env.NEXT_PUBLIC_LOVAC_FRONTEND_URL}`],
   },
   eslint: {
     ignoreDuringBuilds: true,
