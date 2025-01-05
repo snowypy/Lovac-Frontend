b6b935409249976e04e1012769b2144ac846130a
snowypy <git@snowyjs.lol>
2025-01-05 23:07:22 +0000

    fix: update import statement for useRouter to use next/navigation

--- app/tickets/[id]/page.tsx
+++ app/tickets/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { Suspense } from "react";
 import { ChevronLeft, Download } from 'lucide-react';
 import Link from "next/link";
